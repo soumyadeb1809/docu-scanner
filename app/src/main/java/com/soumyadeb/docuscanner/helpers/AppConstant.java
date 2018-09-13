f4514acfd6dbe69e74069706434c3bb6ae5d5ba0
Soumya Deb <soumyadeb1809@gmail.com>
2018-09-13 17:10:45 +0530

    Initial commit

---
+++ app/src/main/java/com/soumyadeb/docuscanner/helpers/AppConstant.java
@@ -0,0 +1,14 @@
+package com.soumyadeb.docuscanner.helpers;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class AppConstant {
+
+    // SD card image directory
+    public static final String PHOTO_ALBUM = "DocuScanner";
+
+    // supported file formats
+    public static final List<String> FILE_EXTN = Arrays.asList("jpg", "jpeg",
+            "png");
+}
